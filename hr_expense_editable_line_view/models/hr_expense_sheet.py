ec460c216abf335ef718e1bd44bf2e9b3d68a5a2
difbit <timo.kekalainen@hotmail.com>
2019-09-20 16:12:56 +0300

    [IMP] Indentation

--- hr_expense_editable_line_view/models/hr_expense_sheet.py
+++ hr_expense_editable_line_view/models/hr_expense_sheet.py
@@ -9,18 +9,18 @@ class HrExpenseSheet(models.Model):
     _inherit = 'hr.expense.sheet'
 
     product_id = fields.Many2one(
-            'product.product',
-            string="Product",
-            related='expense_line_ids.product_id',
-            )
+        'product.product',
+        string="Product",
+        related='expense_line_ids.product_id',
+        )
 
     quantity = fields.Float(
-            string='Quantity',
-            related='expense_line_ids.quantity',
-            )
+        string='Quantity',
+        related='expense_line_ids.quantity',
+        )
 
     unit_amount = fields.Float(
-            string='Unit Price',
-            related='expense_line_ids.unit_amount',
-            )
+        string='Unit Price',
+        related='expense_line_ids.unit_amount',
+        )
 
