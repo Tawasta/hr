5a500dd8fa668ef10058662a16a7e8674e7256a4
difbit <timo.kekalainen@hotmail.com>
2019-09-24 22:10:03 +0300

    [IMP] Lines are now editable

--- hr_expense_editable_line_view/models/hr_expense_sheet.py
+++ hr_expense_editable_line_view/models/hr_expense_sheet.py
@@ -1,25 +1,46 @@
 # -*- coding: utf-8 -*-
 
 
-from odoo import fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class HrExpenseSheet(models.Model):
 
     _inherit = 'hr.expense.sheet'
 
-    product_id = fields.Many2one(
-        'product.product',
-        string="Product",
-        related='expense_line_ids.product_id',
+    state = fields.Selection(selection=[
+        ('draft', 'To Submit'),
+        ('reported', 'Reported'),
+        ('submit', 'Submitted'),
+        ('approve', 'Approved'),
+        ('post', 'Posted'),
+        ('done', 'Paid'),
+        ('cancel', 'Refused'),
+        ],
+        default='draft',
         )
 
-    quantity = fields.Float(
-        string='Quantity',
-        related='expense_line_ids.quantity',
-        )
+    @api.multi
+    def reset_expense_sheets(self):
+        res = super(HrExpenseSheet, self).reset_expense_sheets()
+        self.write({'state': 'draft'})
+        for line in self.expense_line_ids:
+            line.write({'state': 'draft'})
+        return
 
-    unit_amount = fields.Float(
-        string='Unit Price',
-        related='expense_line_ids.unit_amount',
-        )
+    @api.multi
+    @api.onchange('state, product_id')
+    def onchange_line_state(self):
+        for line in self.expense_line_ids:
+            line.state = self.state
+
+    @api.multi
+    def submit_expenses(self):
+        hr_expense = self.env['hr.expense']
+        if any(expense.state != 'draft' for expense in self):
+            raise UserError(_("You cannot report twice the same line!"))
+        if len(self.mapped('employee_id')) != 1:
+            raise UserError(_("You cannot report expenses for different employees in the same report!"))
+        return self.write({'state': 'submit'})
+
